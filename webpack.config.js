5991d0506534b7613459173a688b90a8d6b6a8c2
Jonathan Salis <jon@functionalimperative.com>
2016-05-18 16:52:06 -0400

    updating webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://voxpoplabs.dev:8080',
+    'webpack-dev-server/client?http://localhost:8080',
     'bootstrap-loader',
     './app/'
   ],
